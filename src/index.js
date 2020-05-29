a4f949976aba18a8e896d54d08319456ea9df35f
Anusha Rao <anusha91rao@gmail.com>
2020-05-29 22:13:11 +0530

    Changes made to reflect in-app frontend Routing in github-pages

--- src/index.js
+++ src/index.js
@@ -18,10 +18,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reminders, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-	<Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
